e59f033dafdb0df7c43dad2810171cd73c71a122
2jin2.1031 <2jin2.1031@gmail.com>
2023-12-29 23:31:13 +0900

    refactor: enhance web scraping code for targeted information retrieval

--- festival_crawling/test_crawling.py
+++ festival_crawling/test_crawling.py
@@ -5,15 +5,43 @@ import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 url = "https://www.mcst.go.kr/kor/s_culture/festival/festivalList.jsp?pMenuCD=&pCurrentPage=1&pSearchType=A.FESTIVAL_KORNM&pSearchWord=&pSeq=&pSido=&pOrder=01&pPeriod=&fromDt=&toDt="
+url2 = "https://www.mcst.go.kr/kor/s_culture/festival/festivalList.jsp?pMenuCD=&pCurrentPage=2&pSearchType=A.FESTIVAL_KORNM&pSearchWord=&pSeq=&pSido=&pOrder=01&pPeriod=&fromDt=&toDt="
+
 #response = requests.get(url, verify=False)
 
-html_data = urlopen(url)
+html_data = urlopen(url2)
 
 #데이터가 잘 불러와 오는지 확인 -> sucess
-print(html_data.read())
+#print(html_data.read())
 
 ext_data = BeautifulSoup(html_data, "html.parser")
 title = ext_data.find_all("h2")
 
+
+'''
 for var in title :
     print(var.text)
+'''
+
+festival_info = ext_data.find_all("div", {"class":"text"})
+
+for festival in festival_info:
+    title = festival.find("p", {"class":"title"}).text
+    place = festival.find("ul", {"class":"detail_info"}).text
+    print("제목: ", title)
+    print("장소: ", place)
+    #print(festival.text)
+
+
+'''
+for festival in festival_info:
+    name = festival.find("h2").text
+    print("축제 이름:", name)
+    
+    place = festival.final_all("p")[0].text
+    phone = festival.final_all("p")[1].text
+    
+    print("장소: ", place)
+    print("문의: ", phone)
+    print("-" * 50)
+    '''
